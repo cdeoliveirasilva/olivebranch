4f400cfd1ba919ae8fc6cd814bbfe9e2199b48dd
carla061994 <ca.oliveirasilva@hotmail.com>
2020-05-11 18:24:18 +0200

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import Projects from "./Projects";
 import Contact from "./Contact";
 import Navbar from "./Navbar";
 import { Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/contact" component={Contact} />
-    </div>
+    <Container>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </div>
+    </Container>
   );
 }
 
