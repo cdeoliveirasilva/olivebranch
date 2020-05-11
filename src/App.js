e764d8c6bf0642fc0120dbe92d49372e22b49b20
carla061994 <ca.oliveirasilva@hotmail.com>
2020-05-11 19:09:45 +0200

    Delete Navigation, add Bootstrap Navbar, Links, pages, etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Home from "./Home";
-import About from "./About";
-import Projects from "./Projects";
-import Contact from "./Contact";
-import Navbar from "./Navbar";
-import { Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Navbar from "react-bootstrap/Navbar";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import Nav from "react-bootstrap/Nav";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
-    <Container>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/contact" component={Contact} />
-      </div>
-    </Container>
+    <div className="App">
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <NavbarBrand>C. de Oliveira Silva</NavbarBrand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link classname="nav-link" to="/">
+                  Home
+                </Link>
+                <Link classname="nav-link" to="/about">
+                  About
+                </Link>
+                <Link classname="nav-link" to="/projects">
+                  Projects
+                </Link>
+                <Link classname="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+
+          <Footer />
+        </Container>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/pages/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>This is a footer! Sort of...</p>
+    </footer>
+  );
+}
+
+export default Footer;
