40c60e003e68029441d678b99083131212f5f8d8
carla061994 <ca.oliveirasilva@hotmail.com>
2020-05-13 17:06:42 +0200

    Added project img and Card methods

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+function Card(props) {
+  return <p>Card works!</p>;
+}
+
+export default Card;
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,83 @@
 import React from "react";
 
+import Card from "../components/Card";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import portfolio from "../assets/images/portfolio.png";
+import weatherapi from "../assets/images/weatherapi.jpg";
+import wavegame from "../assets/images/wavegame.jpg";
+
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "React portfolio",
+          subTitle: "React and Bootstrap based portfolio app",
+          imgSrc: portfolio,
+          link: "https://portfolio.domain.com",
+          selected: false,
+        },
+        {
+          id: 2,
+          title: "Weather API",
+          subTitle: "React for fetching worldwide weather information",
+          imgSrc: weatherapi,
+          link: "https://weatherapi.domain.com",
+          selected: false,
+        },
+        {
+          id: 3,
+          title: "The Wave Game",
+          subTitle: "Level-up game built with Java",
+          imgSrc: wavegame,
+          link: "https://wavegame.domain.com",
+          selected: false,
+        },
+      ],
+    };
   }
 
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items];
+
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach((item) => {
+      if (item.id !== id) {
+        item.selected = false;
+      }
+    });
+
+    this.setState({
+      items,
+    });
+  };
+
+  makeItems = (items) => {
+    return items.map((item) => {
+      return (
+        <Card
+          item={item}
+          onClick={(e) => this.handleCardClick(item.id, e)}
+          key={item.id}
+        />
+      );
+    });
+  };
+
   render() {
-    return <p>Carousel works!</p>;
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-around">
+          {this.makeItems(this.state.items)}
+        </Row>
+      </Container>
+    );
   }
 }
 
